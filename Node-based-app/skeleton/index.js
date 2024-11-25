eda9f470242a926e8d3efd4f3add3600406b1f0f
rohit7533 <43840064+rohit7533@users.noreply.github.com>
2024-11-25 11:05:01 +0530

    Update index.js

--- Node-based-app/skeleton/index.js
+++ Node-based-app/skeleton/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from {{parameters.name}}!');
+  res.send("Hello from ${{values.name}}!");
 });
 
 app.listen(port, () => {
